38d0754c28c4811d49fdd16af02a35684d9db523
Amicronpesej8 <Victornahimov@gmail.com>
2023-01-15 23:05:52 +0300

    feat: add blur

--- Plants3/main.js
+++ Plants3/main.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header_navigation')
     const menuClose = document.querySelector('.krestik');
@@ -6,12 +6,27 @@
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header_navigation_active')
     });
-    menuClose.addEventListener('click' , () => {
+    menuClose.addEventListener('click', () => {
         menu.classList.remove('header_navigation_active')
     })
     for (let i = 0; i < menuLinks.length; i++) {
-        menuLinks[i].addEventListener('click' , () => {
+        menuLinks[i].addEventListener('click', () => {
             menu.classList.remove('header_navigation_active')
         })
     }
-}());    
+}());
+
+(function () {
+    const pushedButton = document.querySelector('.button1');
+    const bluredGarden = document.querySelector('.button1');
+    const serviceItem = document.querySelectorAll('.service_item');
+    console.log(serviceItem)
+    serviceItem.forEach(item => {
+        console.log(item);
+    });
+    bluredGarden.addEventListener('click', () => {
+        serviceItem[2].classList.toggle('blured');
+        serviceItem[0].classList.toggle('blured');
+        pushedButton.classList.toggle('button_pushed')
+    });
+}())
